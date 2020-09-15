1823695bba1589481c7c739c1100ec7b84105f2a
likun <zziajkx@gmail.com>
2020-09-15 15:17:51 +0800

    初始化

--- BXMediaCenterSDK.framework/Headers/BXMediaCenterModel.h
+++ BXMediaCenterSDK.framework/Headers/BXMediaCenterModel.h
@@ -15,7 +15,7 @@
 @property (nonatomic,strong) NSString *appKey;
 /// 资源位id
 @property (nonatomic,assign) NSInteger placeId;
-/// 对接类型
+/// 对接服务类型 1小说服务 2活动服务
 @property (nonatomic,assign) NSInteger accessService;
 @end
 
